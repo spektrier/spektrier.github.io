2011150b8e81a8757e8347b1e6175de5c13054f2
Hugo Pereira <a22303324@alunos.ulht.pt>
2024-11-28 08:47:58 +0000

    Full catalog

--- lab9/main.js
+++ lab9/main.js
@@ -37,8 +37,53 @@ price3.textContent = "Custo total: " + produtos[3].price;
 const description3 = document.createElement(`p`);
 description3.textContent = produtos[3].description;
 
-const product3 = document.querySelector(`.product2`);
+const product3 = document.querySelector(`.product3`);
 product3.appendChild(title3);
 product3.appendChild(image3);
 product3.appendChild(price3);
 product3.appendChild(description3);
+
+const title4 = document.createElement(`h3`);
+title4.textContent = produtos[4].title;
+const image4 = document.createElement(`img`);
+image4.src = produtos[4].image;
+const price4 = document.createElement(`p`);
+price4.textContent = "Custo total: " + produtos[4].price;
+const description4 = document.createElement(`p`);
+description4.textContent = produtos[4].description;
+
+const product4 = document.querySelector(`.product4`);
+product4.appendChild(title4);
+product4.appendChild(image4);
+product4.appendChild(price4);
+product4.appendChild(description4);
+
+const title5 = document.createElement(`h3`);
+title5.textContent = produtos[16].title;
+const image5 = document.createElement(`img`);
+image5.src = produtos[16].image;
+const price5 = document.createElement(`p`);
+price5.textContent = "Custo total: " + produtos[16].price;
+const description5 = document.createElement(`p`);
+description5.textContent = produtos[16].description;
+
+const product5 = document.querySelector(`.product5`);
+product5.appendChild(title5);
+product5.appendChild(image5);
+product5.appendChild(price5);
+product5.appendChild(description5);
+
+const title6 = document.createElement(`h3`);
+title6.textContent = produtos[17].title;
+const image6 = document.createElement(`img`);
+image6.src = produtos[17].image;
+const price6 = document.createElement(`p`);
+price6.textContent = "Custo total: " + produtos[17].price;
+const description6 = document.createElement(`p`);
+description6.textContent = produtos[17].description;
+
+const product6 = document.querySelector(`.product6`);
+product6.appendChild(title6);
+product6.appendChild(image6);
+product6.appendChild(price6);
+product6.appendChild(description6);
