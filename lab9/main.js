5ecd47bd957e4e6f2afd0b5c51af29d40798fc0a
spektrier <hugo4lex2004@gmail.com>
2024-11-28 21:46:24 +0000

    Changed main.js to forEach and added styles.css

--- lab9/main.js
+++ lab9/main.js
@@ -1,89 +1,36 @@
-const title1 = document.createElement(`h3`);
-title1.textContent = produtos[1].title;
-const image1 = document.createElement(`img`);
-image1.src = produtos[1].image;
-const price1 = document.createElement(`p`);
-price1.textContent = "Custo total: " + produtos[1].price;
-const description1 = document.createElement(`p`);
-description1.textContent = produtos[1].description;
+document.addEventListener("DOMContentLoaded", () => {
+    carregarProdutos(produtos);
+});
 
-const product1 = document.querySelector(`.product1`);
-product1.appendChild(title1);
-product1.appendChild(image1);
-product1.appendChild(price1);
-product1.appendChild(description1);
+function carregarProdutos(produtos) {
+    const container = document.querySelector("#productsContainer"); 
 
-const title2 = document.createElement(`h3`);
-title2.textContent = produtos[2].title;
-const image2 = document.createElement(`img`);
-image2.src = produtos[2].image;
-const price2 = document.createElement(`p`);
-price2.textContent = "Custo total: " + produtos[2].price;
-const description2 = document.createElement(`p`);
-description2.textContent = produtos[2].description;
+    produtos.forEach(produto => {
+        console.log(produto.id, produto.title);
+        const article = criarProduto(produto); 
+        container.appendChild(article); 
+    });
+}
 
-const product2 = document.querySelector(`.product2`);
-product2.appendChild(title2);
-product2.appendChild(image2);
-product2.appendChild(price2);
-product2.appendChild(description2);
+function criarProduto(produto) {
+    const article = document.createElement("article");
 
-const title3 = document.createElement(`h3`);
-title3.textContent = produtos[3].title;
-const image3 = document.createElement(`img`);
-image3.src = produtos[3].image;
-const price3 = document.createElement(`p`);
-price3.textContent = "Custo total: " + produtos[3].price;
-const description3 = document.createElement(`p`);
-description3.textContent = produtos[3].description;
+    const title = document.createElement("h3");
+    title.textContent = produto.title;
 
-const product3 = document.querySelector(`.product3`);
-product3.appendChild(title3);
-product3.appendChild(image3);
-product3.appendChild(price3);
-product3.appendChild(description3);
+    const image = document.createElement("img");
+    image.src = produto.image;
 
-const title4 = document.createElement(`h3`);
-title4.textContent = produtos[4].title;
-const image4 = document.createElement(`img`);
-image4.src = produtos[4].image;
-const price4 = document.createElement(`p`);
-price4.textContent = "Custo total: " + produtos[4].price;
-const description4 = document.createElement(`p`);
-description4.textContent = produtos[4].description;
+    const description = document.createElement("p");
+    description.textContent = produto.description;
 
-const product4 = document.querySelector(`.product4`);
-product4.appendChild(title4);
-product4.appendChild(image4);
-product4.appendChild(price4);
-product4.appendChild(description4);
+    const price = document.createElement("p");
+    price.textContent = `Custo Total: ` + produto.price + `€`;
 
-const title5 = document.createElement(`h3`);
-title5.textContent = produtos[16].title;
-const image5 = document.createElement(`img`);
-image5.src = produtos[16].image;
-const price5 = document.createElement(`p`);
-price5.textContent = "Custo total: " + produtos[16].price;
-const description5 = document.createElement(`p`);
-description5.textContent = produtos[16].description;
+    article.appendChild(title);
+    article.appendChild(image);
+    article.appendChild(description);
+    article.appendChild(price);
 
-const product5 = document.querySelector(`.product5`);
-product5.appendChild(title5);
-product5.appendChild(image5);
-product5.appendChild(price5);
-product5.appendChild(description5);
-
-const title6 = document.createElement(`h3`);
-title6.textContent = produtos[17].title;
-const image6 = document.createElement(`img`);
-image6.src = produtos[17].image;
-const price6 = document.createElement(`p`);
-price6.textContent = "Custo total: " + produtos[17].price;
-const description6 = document.createElement(`p`);
-description6.textContent = produtos[17].description;
-
-const product6 = document.querySelector(`.product6`);
-product6.appendChild(title6);
-product6.appendChild(image6);
-product6.appendChild(price6);
-product6.appendChild(description6);
+    return article;
+}
